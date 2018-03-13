80ab69fa7412764b90e8b9f9a53a99f6338014fc
compostbrain <joelcahalan@gmail.com>
2018-03-13 18:07:20 -0400

    init repo

---
+++ spec/type-code-faster-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TypeCodeFasterView from '../lib/type-code-faster-view';
+
+describe('TypeCodeFasterView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
